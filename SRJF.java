bb170381206830e399ad4bf4f59b037daa3b092f
Nikiman <nikimangroup@gmail.com>
2017-11-10 11:51:40 +0300

    Basic GUI

--- SRJF.java
+++ SRJF.java
@@ -1,14 +1,89 @@
+import java.util.function.UnaryOperator;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  * SRJF
  */
-public class SRJF {
+public class SRJF extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Shortest Remaining Time First ");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahoma",FontWeight.BOLD,20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+        
+        Label noOfJobsLabel = new Label("Enter Number of Jobs:");
+        grid.add(noOfJobsLabel, 0, 1);
+
+        UnaryOperator<Change> filter = change -> {
+            String text = change.getText();
+
+            if (text.matches("[0-9]*")) {
+                return change;
+            }
+
+            return null;
+        };
+        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+
+        TextField jobTextField = new TextField();
+        grid.add(jobTextField, 1, 1);
+
+        Button btn = new Button("Enter");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(btn, 1, 3);
+
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 5);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                actionTarget.setFill(Color.FORESTGREEN);
+                Integer noOfJobs = Integer.valueOf(jobTextField.getText());
+                actionTarget.setText("Creating "+noOfJobs+" processes");
+
+            }
+        });
+
+        Scene scene = new Scene(grid, 400, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
+
+/**
+ * Job class
+ * Collects details about the jobs
+ */
+class Job {
+
 }
