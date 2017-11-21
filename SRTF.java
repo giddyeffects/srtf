f4a7e1f8394e2fc25154ca92fb4adbb2ba6e0aba
Nikiman <nikimangroup@gmail.com>
2017-11-21 12:40:55 +0300

    Adding WT TT processing

--- SRTF.java
+++ SRTF.java
@@ -1,4 +1,4 @@
-//import java.awt.TextField;
+//import SRTF.Job;
 import java.util.function.UnaryOperator;
 import java.util.Optional;
 import javafx.application.Application;
@@ -21,6 +21,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -38,9 +39,10 @@ public class SRTF extends Application {
     Scene scene1, scene2;
     //declare the  application grids
     GridPane grid1 = new GridPane(), grid2 = new GridPane();
-    TableView processTable = new TableView();
+    TableView<Job> processTable = new TableView<Job>();
+    ObservableList<Job> data;
     Stage thestage, newStage;
-    int counter2=0;
+    int counter2=0,totalTime=0;
     Job[] JobsArray;
 
     public static void main(String[] args) {
@@ -60,7 +62,7 @@ public class SRTF extends Application {
         grid1.setVgap(10);
         grid1.setPadding(new Insets(25, 25, 25, 25));
 
-        Text sceneTitle = new Text("Welcome to SRTF");
+        Text sceneTitle = new Text("Welcome to SRTF Simulator");
         sceneTitle.setFont(Font.font("Tahoma",FontWeight.BOLD,20));
         grid1.add(sceneTitle, 0, 0, 2, 1);
         
@@ -71,14 +73,15 @@ public class SRTF extends Application {
         grid1.add(actionTarget, 1, 5);
         processTable.setEditable(false);
 
-        TableColumn jobNoCol = new TableColumn("Process No.");
+        TableColumn<Job, Integer> jobNoCol = new TableColumn<Job, Integer>("Process No.");
         jobNoCol.setCellValueFactory(new PropertyValueFactory<Job, Integer>("jobNo"));
-        TableColumn cpuTimeCol = new TableColumn("CPU Burst Time");
+        TableColumn<Job, Integer> cpuTimeCol = new TableColumn<Job, Integer>("CPU Burst Time");
         cpuTimeCol.setMinWidth(100);
         cpuTimeCol.setCellValueFactory(new PropertyValueFactory<Job, Integer>("cpuTime"));
-        TableColumn arrivalTimeCol = new TableColumn("Arrival Time");
+        TableColumn<Job, Integer> arrivalTimeCol = new TableColumn<Job, Integer>("Arrival Time");
         arrivalTimeCol.setCellValueFactory(new PropertyValueFactory<Job, Integer>("arrivalTime"));
 
+        processTable.setItems(data);
         processTable.getColumns().addAll(jobNoCol, cpuTimeCol, arrivalTimeCol);
 
         grid1.add(processTable, 0, 6, 2, 1);
@@ -96,6 +99,9 @@ public class SRTF extends Application {
         TextFormatter<String> textFormatter = new TextFormatter<>(filter);
 
         TextField jobTextField = new TextField();
+        Tooltip tooltip = new Tooltip();
+        tooltip.setText("Enter an integer\n Between 1 to 5\n");
+        jobTextField.setTooltip(tooltip);
         jobTextField.setTextFormatter(textFormatter);
         jobTextField.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -154,6 +160,7 @@ public class SRTF extends Application {
         grid1.add(jobTextField, 1, 1);
 
         Button btnScene1 = new Button("Enter");
+        //btnScene1.setStyle("-fx-background-color: darkslateblue; -fx-textfill: white;");
         HBox hbBtn = new HBox(10);
         hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
         hbBtn.getChildren().add(btnScene1);
@@ -170,6 +177,7 @@ public class SRTF extends Application {
         });
 
         scene1 = new Scene(grid1, 400, 400);
+        jobTextField.requestFocus();
         primaryStage.setScene(scene1);
         primaryStage.show();
     }//close start function
@@ -245,18 +253,61 @@ public class SRTF extends Application {
                     counter2++;
                     Integer arrTime = Integer.valueOf(inputTextField[counter2].getText());
                     JobsArray[i] = new Job(jn, cpuTime, arrTime);
+                    totalTime += cpuTime;//get total execution time
                     counter2++;
                 }
                 processJobs(JobsArray);
+                
+
+                for (int i = 0; i < num; i++) {
+                    System.out.println("Turn around time for process "+(i+1)+": "+JobsArray[i].getTT()+" and waiting time is "+JobsArray[i].getWT());
+                }
+
             }
         });
         grid.add(btnScene1, 1, counter);
+
         return grid;
     }//close createProcessPane function
 
     public void processJobs(Job[] JobsArray) {
-        ObservableList<Job> data = FXCollections.observableArrayList(JobsArray);
+        data = FXCollections.observableArrayList(JobsArray);
         processTable.setItems(data);
+        for (int i = 0; i < totalTime; i++) {
+            //select shortest process which has arrived
+            int activeProcess = 0;//currently active process Index
+            int minCpuTime = 99999; //current least cpu time... put initial large number as the shortest cpu time
+            for (int j = 0; j < JobsArray.length; j++) {
+                //check if the process has arrived in the timeline 'i'
+                if(JobsArray[j].getArrivalTime() <= i ) {
+                    //if the job's cpu time is less than the current least cpu time i.e. is the shortest time, and not equal to zero
+                    if(JobsArray[j].getCpuTime() < minCpuTime && JobsArray[j].getCpuTime() !=0) {
+                        activeProcess = j; //the index of active job
+                        minCpuTime = JobsArray[activeProcess].getCpuTime();
+                    }
+                }
+            }
+            //@TODO create gantt chart
+
+            //decrement remaining time of selected process by 1 since it has been assigned 1 unit of cpu time
+            JobsArray[activeProcess].decrementCT(1);
+
+            //@TODO see how to combine these two repeating loops
+            //calculate Waiting Time(WT) and Turn Around time(TT)
+            for (int j = 0; j < JobsArray.length; j++) {
+                //check if the process has arrived in the timeline 'i'
+                if (JobsArray[j].getArrivalTime() <= i) {
+                    if (JobsArray[j].getCpuTime() != 0) {
+                        //if process has arrived but it's not completed TT is incremented by 1
+                        JobsArray[j].turnAroundTime++;
+                        //if process has not been currently assigned the CPU but has arrived increment WT
+                        if(j != activeProcess) JobsArray[j].waitingTime++;
+                    }
+                    else if (j == activeProcess)//current process has been assigned CPU but cpu time 
+                        JobsArray[j].turnAroundTime++;//@TODO confirm this
+                }
+            }
+        }
     }
 
     /**
@@ -265,29 +316,43 @@ public class SRTF extends Application {
     */
     public static class Job {
         private SimpleIntegerProperty jobNo, cpuTime, arrivalTime, finishTime, startTime;
+        private int turnAroundTime = 0, waitingTime = 0;
 
         /** Job class constructor */
         public Job(int job, int burst, int atime) {
-            jobNo = new SimpleIntegerProperty(job);
-            cpuTime = new SimpleIntegerProperty(burst);
-            arrivalTime = new SimpleIntegerProperty(atime);
+            this.jobNo = new SimpleIntegerProperty(job);
+            this.cpuTime = new SimpleIntegerProperty(burst);
+            this.arrivalTime = new SimpleIntegerProperty(atime);
         }
 
         /** Getter & Setter methods below */
 
-        public int turnAroundTime() {
+        /*public int turnAroundTime() {
             return finishTime.get() - arrivalTime.get();
+        }*/
+
+        public int getTT() {
+            return turnAroundTime;
+        }
+
+        public int getWT() {
+            return waitingTime;
         }
 
         public int getArrivalTime() {
             return arrivalTime.get();
         }
 
-        public int getCPUTime() {
+        public int getCpuTime() {
             return cpuTime.get();
         }
 
-        public int getJobNumber() {
+        public void decrementCT(int no) {
+
+            this.cpuTime.set(cpuTime.get() - no);
+        }
+
+        public int getJobNo() {
             return jobNo.get();
         }
 
